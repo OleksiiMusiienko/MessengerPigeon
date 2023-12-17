9a90d2f384048411d77d7ae9786cb78501da0935
OleksiiMusiienko <musienko1984@gmail.com>
2023-12-17 11:55:00 +0200

    #Task1 Создание класса Message
    Класс описывает обьект сообщения

---
+++ MessengerModel/ClassMessage.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessengerModel
+{
+    class Message
+    {
+        public string Mes {  get; set; }
+        public DateTime Date_Time {  get; set; } 
+        public Message(string mes, DateTime date_Time)
+        {
+            Mes = mes;
+            Date_Time = DateTime.Now;
+        }
+    }
+}
