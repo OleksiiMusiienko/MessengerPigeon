9e7dabcb5cc5456b4e95f7f972e5b1e440a490c3
OleksiiMusiienko <musienko1984@gmail.com>
2023-12-17 11:55:56 +0200

    #Task2 Создание класса User
    Класс описывает пользователя

---
+++ MessengerModel/ClassUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessengerModel
+{
+    class User
+    {
+        public string Nick {  get; set; }
+        public string Password { get; set; }
+        public string Phone {  get; set; }      
+        public byte[]? Avatar { get; set; }
+        public User(string nick, string password, string phone, byte[] avatar)
+        {
+            Nick = nick;
+            Password = password;
+            Phone = phone;
+            Avatar = avatar;
+        }
+        //public string Mail {  get; set; }// обсудить возможность подтверждения аккаунта по почте
+    }
+}
