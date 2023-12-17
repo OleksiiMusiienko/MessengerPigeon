0a6edc720519f0b8e016c1f1e592f0b61fa8faee
OleksiiMusiienko <musienko1984@gmail.com>
2023-12-17 16:36:09 +0200

    #Task3 дополнение класса Message
    Добавление поля ID и навигационного свойства для связи многие ко многим
    Добавление полей Id для отправителя и получателя

--- MessengerModel/ClassMessage.cs
+++ MessengerModel/ClassMessage.cs
@@ -9,12 +9,17 @@ namespace MessengerModel
 {
     class Message
     {
-        public string Mes {  get; set; }
-        public DateTime Date_Time {  get; set; } 
+        public int Id { get; set; } //not null
+        public DateTime Date_Time {  get; set; } //not null
+        public int UserSenderId {  get; set; } //not null
+        public int UserRecepientId { get; set; } //not null
+        public string Mes {  get; set; } //not null
+
         public Message(string mes, DateTime date_Time)
         {
             Mes = mes;
             Date_Time = DateTime.Now;
         }
+        public ICollection<User> Users { get; set; }
     }
 }
