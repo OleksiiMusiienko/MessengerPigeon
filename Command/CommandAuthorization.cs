e3075bba8a1818dcbe4270a008d334b7d4de2e60
OleksiiMusiienko <musienko1984@gmail.com>
2023-12-20 16:31:35 +0200

    #Task11 Добавление класса CommandAuthorization
    Класс отвечает за реализацию команды авторизации пользователя и доступности кнопки

---
+++ Command/CommandAuthorization.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MessengerPigeon.Command
+{
+    class CommandAuthorization: ICommand
+    {
+        public CommandAuthorization(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute != null)
+            {
+                return _canExecute(parameter);
+            }
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        Action<object> _execute;
+        Predicate<object> _canExecute;
+    }
+}
