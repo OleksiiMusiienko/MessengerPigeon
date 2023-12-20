302c06948bdeae1bb3c2f621904688570bc4ea47
IrynaKonovalowa <iri.moummi@gmail.com>
2023-12-20 19:10:53 +0100

    #Task17 Создание класса MessengerViewModel
    Обернуты свойства Nick, Password, Avatar для класса User.
    И свойства Date_Time, Mes для класса Message.
    Созданы 2 ObservableCollection для User и Message.
    Реализован контракт INotifyPropertyChanged.
    
    Класс MessengerViewModel является посредником между моделью и представлением.

---
+++ MessengerViewModel.cs
@@ -0,0 +1,100 @@
+﻿using MessengerModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessengerPigeon
+{
+    class MessengerViewModel : INotifyPropertyChanged
+    {
+        private User User;
+        private Message Message;
+
+        public string Nick
+        {
+            get { return User.Nick; }
+            set
+            {
+                User.Nick = value;
+                OnPropertyChanged(nameof(Nick));
+            }
+        }
+
+        public string Password
+        {
+            get { return User.Password; }
+            set
+            {
+                User.Password  = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+        
+        public byte[]? Avatar
+        {
+            get { return User.Avatar; }
+            set
+            {
+                User.Avatar   = value;
+                OnPropertyChanged(nameof(Avatar));
+            }
+        }
+
+        public DateTime Date_Time
+        {
+            get { return Message.Date_Time; }
+            set
+            {
+                Message.Date_Time = value;
+                OnPropertyChanged(nameof(Date_Time));
+            }
+        }
+        public string Mes
+        {
+            get { return Message.Mes; }
+            set
+            {
+                Message.Mes  = value;
+                OnPropertyChanged(nameof(Mes));
+            }
+        }
+
+        private ObservableCollection<User> _users = new ObservableCollection<User>();
+        public ObservableCollection<User> Users
+        {
+            get
+            {
+                return _users;
+            }
+            set
+            {
+                _users = value;
+                OnPropertyChanged(nameof(Users));
+            }
+        }
+
+        private ObservableCollection<Message> _messages = new ObservableCollection<Message>();
+        public ObservableCollection<Message> Messages
+        {
+            get
+            {
+                return _messages;
+            }
+            set
+            {
+                _messages = value;
+                OnPropertyChanged(nameof(Messages));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
